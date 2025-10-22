23724c06eaa0978d73c7697d1ea0845095460f73
Frank Peng <frankp5@illinois.edu>
2025-10-22 17:14:12 -0500

    found a new way to implement calendar feature

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+
 //import 'package:helloworld/sign-in-page.dart';
-import 'package:helloworld/calendar.dart';
-
+//import 'package:helloworld/calendar.dart';
+import 'package:helloworld/calendar_lib.dart';
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Calendar(), // start with Sign-In page
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new_calendar(), // start with Sign-In page
+    ),
+  );
 }
-
