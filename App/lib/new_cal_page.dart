55c0514c46380156484929ceec3ad7da1a0414d6
Frank Peng <frankp5@illinois.edu>
2025-11-02 16:01:19 -0600

    created the new calendar page

--- App/lib/new_cal_page.dart
+++ App/lib/new_cal_page.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:helloworld/sign-in-page.dart';
 
+class new_cal extends StatefulWidget {
+  const new_cal({super.key});
+
+  @override
+  State<new_cal> createState() => _new_calState();
+}
+
+class _new_calState extends State<new_cal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Calendar Page'),
+      ),
+      body: const Center(
+        child: Text('This is the new calendar page.'),
+      ),
+    );
+  }
+}
